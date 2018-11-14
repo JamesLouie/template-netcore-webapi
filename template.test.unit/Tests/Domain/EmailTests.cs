527aa9ed37a0bc6e30b6e393e4dd17c0c57dc0f7
James Louie <Jamlouie12@gmail.com>
2018-11-13 20:58:59 -0800

    - Add unit test project

---
+++ template.test.unit/Tests/Domain/EmailTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using template.Domain.ValueObjects;
+using Xunit;
+
+namespace template.test.unit.Tests.Domain
+{
+    public class EmailTests
+    {
+        [Fact]
+        public void Constructor_StandardInput()
+        {
+            // Setup
+            var standardInput = "test@test.com";
+
+            // Test
+            var email = new Email(standardInput);
+
+            // Validate
+            Assert.Equal("test", email.LocalName);
+            Assert.Equal("test.com", email.DomainAddress);
+            Assert.Equal(standardInput, email.CompleteEmailAddress);
+        }
+
+        [Fact]
+        public void Constructor_DoesNotContainSymbol_ThrowsException()
+        {
+            // Setup
+            var invalidInput = "testtest.com";
+
+            // Test and Validate
+            Assert.Throws<ArgumentException>(() => new Email(invalidInput));
+        }
+    }
+}
